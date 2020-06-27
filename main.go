292164dcf229e6575fce39677908bc2c9b1ab497
gopye <gopye@tuta.io>
2020-06-27 16:17:43 -0700

    scan all branches

--- main.go
+++ main.go
@@ -10,11 +10,10 @@ import (
 	"regexp"
 	"sort"
 	"strings"
-	// "strconv"
+	"errors"
 
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing"
-	// fdiff "github.com/go-git/go-git/v5/plumbing/format/diff"
 	"github.com/go-git/go-git/v5/plumbing/object"
 	"github.com/go-git/go-git/v5/storage/memory"
 )
@@ -44,6 +43,17 @@ func main() {
 
 		ref, err := r.Head()
 		checkIfError(err)
+
+		b, err := r.Branches()
+		checkIfError(err)
+
+		iter := 0
+		b.ForEach(func(r *plumbing.Reference) error {
+			fmt.Println(iter)
+			iter++
+			fmt.Println(r)
+			return nil
+		})
 
 		// ... retrieving the commit object
 		commit, err := r.CommitObject(ref.Hash())
@@ -94,6 +104,7 @@ func main() {
 		fmt.Printf("TOTAL STRINGS = %s\n", len(list))
 		// ... retrieve the tree from the commit
 		fmt.Printf("commit parent hashes: %s\n", commit.ParentHashes)
+
 	} else if dir != "unset" {
 
 		repo, err := git.PlainOpen(dir)
@@ -106,32 +117,34 @@ func main() {
 		commit, err := repo.CommitObject(ref.Hash())
 		checkIfError(err)
 
-		// add commit to work struct
-		tempWork = append(tempWork, commit.Hash)
-		fmt.Println("~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-		fmt.Println(tempWork)
-		fmt.Println("~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-
-		// pas in work struct
 		// scans every file in current branch HEAD
 		scanCommitTree(commit, dir)
 		checkIfError(err)
 
 		scanParentCommits(commit, repo)	
-		
 
-		var keys []string // I think we need this so we can sort.
+		refs, err := repo.References()
+		checkIfError(err)
+
+		refs.ForEach(func(ref *plumbing.Reference) error {
+			if (ref.Type() == plumbing.HashReference ) {
+				commit, err := repo.CommitObject(ref.Hash())
+				checkIfError(err)
+
+				scanCommitTree(commit, dir)
+				checkIfError(err)
+
+				scanParentCommits(commit, repo)	
+			}
+
+			return nil
+		})
+
+		var keys []string
 		for k, _ := range list {
 			keys = append(keys, k)
-			// list := map[string][]map[string]string{}
-			// fmt.Println("k:", k)
-			// for _, v1 := range v[1] {
-			// 	fmt.Println("v:", strings.TrimSpace(v1))
-			// }
-			// fmt.Println("------------")
 		}
 
-		// should be at very end but don't want to see spam
 		sort.Sort(ByLen(keys))
 		i := 0
 		for i < len(keys) {
@@ -139,17 +152,11 @@ func main() {
 			i++
 		}
 
-
-
-		fmt.Println(completed)
 		fmt.Printf("\nTOTAL STRINGS = %s\n", len(list))
-
-
 
 	} else {
 		fmt.Println("Error handling flags")
 	}
-
 }
 
 type hit struct {
@@ -157,7 +164,6 @@ type hit struct {
 	line     string
 }
 
-var tempWork = make([]plumbing.Hash, 1) // this seems fishy
 var work = make(map[plumbing.Hash][]plumbing.Hash)
 var completed = make(map[plumbing.Hash][]plumbing.Hash)
 var list = map[string][]map[string]string{}
@@ -214,11 +220,10 @@ func info(format string, args ...interface{}) {
 }
 
 func scanCommitTree(c *object.Commit, dir string) error {
-	// ... retrieve the tree from the commit
-	tree, err := c.Tree()
+	cIter, err := c.Files()
 	checkIfError(err)
 
-	tree.Files().ForEach(func(file *object.File) error {
+	cIter.ForEach(func(file *object.File) error {
 		// check for file extentions we don't want to inspect
 		if excludeExtRe.Match([]byte(file.Name)) {
 			return nil
@@ -228,7 +233,8 @@ func scanCommitTree(c *object.Commit, dir string) error {
 		fullpath := dir + "/" + file.Name
 		f, err := os.Open(fullpath)
 		if err != nil {
-			fmt.Printf("CANNOT OPEN: %s", file.Name)
+			// fmt.Printf("CANNOT OPEN: %s\n", fullpath)
+			return nil
 		}
 		defer f.Close()
 
@@ -255,39 +261,15 @@ func scanCommitTree(c *object.Commit, dir string) error {
 
 
 func scanParentCommits(c *object.Commit, r *git.Repository) {
-	fmt.Println(c.ParentHashes)
-	// fmt.Println
 	if len(c.ParentHashes) == 0 {
 		return
 	}
-	// verify work has not already been done
-
-	// fmt.Println("~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-	// fmt.Println(completed)
-	// fmt.Println("~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-
-
 
 	for _, parentHash := range c.ParentHashes {
-		if completed[c.Hash] != nil {
-			fmt.Println("\n\nFOUND \n\n")
-			fmt.Println(c.Hash)
-			fmt.Printf("%T\n", c.Hash)
-			fmt.Println(completed[c.Hash])
-			fmt.Printf("%T\n", completed[c.Hash])
-			fmt.Println("~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-			return
-		}
+		if completed[c.Hash] != nil { return }
 
 		parentCommit, err := r.CommitObject(parentHash)
 		checkIfError(err)
-
-		fmt.Println(parentCommit)
-		// work[c.Hash] = c.ParentHashes
-
-		// for _, parentHash := range work[c.Hash] {
-		// 	parentCommit, err := r.CommitObject(parentHash)
-		// 	checkIfError(err)
 
 		parentTree, err := parentCommit.Tree()
 		checkIfError(err)
@@ -299,8 +281,10 @@ func scanParentCommits(c *object.Commit, r *git.Repository) {
 		changes, diffErr := object.DiffTree(currentTree, parentTree)
 		checkIfError(diffErr)
 
+		// no change, does this ever trigger?
 		if len(changes) == 0 {
 			completed[c.Hash] = append(completed[c.Hash], parentHash)
+			errors.New("NO CHANGES IN DIFFS")
 			continue 
 		}
 
@@ -329,9 +313,7 @@ func scanParentCommits(c *object.Commit, r *git.Repository) {
 				}
 			}
 		}
-		// add commit -> parentCommit hashes to work done struct
 		completed[c.Hash] = append(completed[c.Hash], parentHash)
 		scanParentCommits(parentCommit, r)
 	}
-	// can I recursively call this function?
 }
